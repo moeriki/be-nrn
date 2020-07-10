22f78ee530491e5fa12a8efbddbdcb0756ef5dbc
Nikolay <nikolay@foursevens.be>
2020-07-10 12:53:01 +0200

    padding between the tests added

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -34,14 +34,17 @@ describe('getBirthDate()', () => {
       parseDate('2001-08-14').getTime(),
     );
   });
+
   it('should extract the correct birthdate from a NRN', () => {
     expect(nrnUtils.getBirthDate('810212 896 71')).toEqual(parseDate('1981-02-12'));
   });
+
   it('should extract the correct birthdate from a BIS number', () => {
     expect(nrnUtils.getBirthDate('814212 896 60')).toEqual(parseDate('1981-02-12'));
     expect(nrnUtils.getBirthDate('812212 896 17')).toEqual(parseDate('1981-02-12'));
     expect(nrnUtils.getBirthDate('810000 896 29')).toEqual(parseDate('1981-06-01'));
   });
+
   it('should always parse in Belgian timezone', () => {
     // This test depends on the timezone's current computer.
     // If it succeeds locally (running on a computer in Belgian timezone),
